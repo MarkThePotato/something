4bb5071fcd7d38ecbd616199805a6cd9f6af1542
MarkThePotato <94894447+MarkThePotato@users.noreply.github.com>
2023-12-04 23:33:19 +0300

    Add files via upload

---
+++ Learning/main.py
@@ -0,0 +1,112 @@
+import math
+import time
+
+#x = False
+#y = "joe"
+#z = int(2)
+
+#print(x, z, y)
+
+#car_name = "Mazada 6"
+#car_age = abs(2016-2023)
+#car_length = 4800.29
+
+#print("The car name: "+car_name+" the car is "+str(car_age)+" years old. The length of the car is "+str(car_length)+"mm")
+
+#name = input("State your name here please: ")
+#print("Nice to meet you, "+name+"!")
+#age = int(input("How old are you: "))
+#print("So, you're "+str(age)+" years old... Interesting.")
+#car = input("What's your car?")
+#print("Nice one.\nthank you for your time sir!")
+
+#sentence = "La Mujer Come El Gato"
+#sentence1 = sentence[0:8].lower
+#print(sentence1)
+
+#age = int(input("State how old are you please: "))
+
+#if age == 100 or age >= 101:
+    #print("How are you alive?")
+#elif age >= 18:
+    #print('You seem to be an adult.')
+#else:
+    #print("You either have not been born yet, or you're a minor. and no, 17 does not count as an adult.");
+
+#response = input("Would you like to order some food? (Y/N): ")
+
+#if response == "Y" or response == "y":
+    #print("Here! Have some food.")
+#elif response == "N" or response == "n":
+    #print("Okay, i'll leave then.")
+#else:
+    #print("Your answer should be N or Y only.")
+
+#name = input("enter your name: ")
+
+#if name == "":
+    #print("The name can not be empty.")
+#if name.isdigit:
+    #print("Your name can not be digits only.")
+#else:
+    #print("Interesting name.")
+
+#options = int(input("If you wan to convert KG to LBS then type 1. If you want to convert LBS to KG then type 2:  "))
+
+#if options == 1:
+    #KG = float(input("Enter how much KGs you want to convert."))
+    #print(KG*2.2)
+#elif options == 2:
+    #LBS = float(input("Enter how much pounds you want to convert."))
+    #print(LBS/2.2)
+#else:
+    #print("something went wrong. You sure you put 1 or 2?")
+
+#num1 = int(input("Input your number: "))
+#num2 = int(input("enter your second number: "))
+#operator_option = input("Which operator will you use? only *, -, +, / are available: ")
+
+#if operator_option == "*":
+    #print(num1*num2)
+#elif operator_option == "/":
+    #print(num1/num2)
+#elif operator_option == "+":
+    #print(num1+num2)
+#elif operator_option == "-":
+    #print(num1-num2)
+#else:
+    #print("Choose a valid operator")
+
+# name = input("Enter your name please: ")
+
+# while name.isdigit():
+    # print("Your name can not be digits only.")
+    # name = input("Enter your name please: ")
+# while name == None  or name == "":
+    # print("Your name can not be empty")
+    # name = input("Enter your name please: ")
+# print(f"You're signed up now, {name}!")
+
+# age = int(input("How old are you?: "))
+
+# while age == None:
+    # print("You can't leave this field")
+    # age = int(input("How old are you?: "))
+# while age < 0:
+    # print("Cap.")
+    # age = int(input("How old are you?: "))
+# print("nice")
+
+# something = ['pizza', 'mama', 'el gato']
+
+# for somethin in something:
+    # print(somethin)
+# rows = int(input("How many rows?: "))
+# columns = int(input("How many rows?: "))
+# symbol = input("Input your symbol")
+
+# while 1 == 1:
+#     for row in range(rows):
+#         for column in range(columns):
+#             print(symbol, end="")
+#         print()
